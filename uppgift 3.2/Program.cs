e5ee006e724dda72ddba33c068e54b536b1f732e
Admin <Admin@TEB123016>
2023-10-20 15:26:22 +0200

    Add project files.

---
+++ uppgift 3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace uppgift_3._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("har du gått ut gymnsaiumet");
+            string x = (string)Console.ReadLine();
+            if (x=="j")
+            {
+
+            }
+            string z = (string)Console.ReadLine();
+            if (z=="n")
+            {
+
+            }
+            Console.WriteLine("hur gammal är du ");
+            int y = int.Parse(Console.ReadLine());
+            if (y < 22)
+            {
+                Console.WriteLine("du får jobb");
+            }
+            int a = int.Parse(Console.ReadLine());
+            if (a>22)
+            {
+                Console.WriteLine("du får inte jobb");
+            }
+        }
+    }
+}
+
